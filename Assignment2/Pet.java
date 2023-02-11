e57e337acfae59791ff2f91c3ea1e4b1e10dc087
evancherewko <113801945+evancherewko@users.noreply.github.com>
2023-02-10 20:06:24 -0700

    Add files via upload

---
+++ Assignment2/Pet.java
@@ -0,0 +1,41 @@
+public class Pet {
+    //Object Variables
+    private final String NAME;
+    private final String SPECIES;
+    private final String BREED;
+    private final String COLOUR;
+
+    private boolean vaccineStatus = false;
+    private Client owner;
+    private EmergVet vet;
+    private CareProfile care;
+
+
+    //Constructor
+    public Pet(String name, String species, String breed, String colour, Client owner){
+
+    }
+
+    //Helper Functions
+
+
+    //Getters
+    public setVet(EmergVet vet)
+
+    //Setters
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
